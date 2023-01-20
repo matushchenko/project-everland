ea64530c63d4febad67fef7081990be55fd7788d
Kristina Ushakova <mramornayaa@yandex.ru>
2023-01-20 17:07:39 +0300

    all

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,14 @@ const popupPeopleVisible = document.querySelector('#people')
 const popupService = document.querySelector('#popup-hide-service')
 const popupServiceVisible = document.querySelector('#service')
 
+const description = document.querySelector(".description");
+const descriptionContainer = document.querySelector(".description__container");
+const descriptionSlider = description.querySelectorAll(".description__slider");
+const descriptionLeft = document.querySelector(".description__left");
+const descriptionRight = document.querySelector(".description__right");
+const descriptionNumbers = document.querySelector(".description__numbers");
+let offset = 0;
+
 //фунция для закрытия развернутого в меню
 function closeAll(esc) {
   if (esc.classList.contains('popup__icon_reverse')) {
@@ -86,3 +94,51 @@ popupMenuClose.addEventListener('click', function () {
   closeAll(popupPeople);
   closeAll(popupService);
 })
+
+/*Перелистывание слайдов*/
+
+descriptionRight.addEventListener("click", function () {
+  offset = offset + 1440;
+  if (offset > 7200) {
+    offset = 0;
+  }
+  descriptionContainer.style.left = -offset + "px";
+  return offset;
+});
+
+descriptionLeft.addEventListener("click", function () {
+  offset = offset - 1440;
+  if (offset < 0) {
+    offset = 7200;
+  }
+  descriptionContainer.style.left = -offset + "px";
+  return offset;
+});
+
+/*Счетчик перелистывания*/
+
+let savedIndex = 1;
+
+function reducingNumber() {
+  descriptionNumbers.textContent = `${(savedIndex += 1)}/${6}`;
+  if (savedIndex >= 6) {
+    savedIndex = 0;
+  }
+}
+
+descriptionRight.addEventListener("click", function () {
+  reducingNumber();
+});
+
+function increasingNumber() {
+  descriptionNumbers.textContent = `${(savedIndex -= 1)}/${6}`;
+  if (savedIndex >= 6) {
+    savedIndex = 6;
+  } else if (savedIndex <= 1) {
+    savedIndex = 7;
+  }
+}
+
+descriptionLeft.addEventListener("click", function () {
+  increasingNumber();
+});
