fb7802b210617eb0d2c870bf8b4230bd8c2204ae
Denis Ivanov <site.ivanov201@yandex.ru>
2023-01-21 22:28:20 +0300

    2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,95 @@
+//объявление
 const boxTemplate = document.querySelector('#element-template').content;
 const elementsBox = document.querySelector('.advantages__container');
+const popupMenu = document.querySelector('#popup-menu');
+const popupMenuSwitch = document.querySelector('#menu-bottom');
+const icon = document.querySelector('.header__button-img');
+
+const popupProject = document.querySelector('#popup-hide-project');
+const popupProjectVisible = document.querySelector('#project');
+
+const popupPeople = document.querySelector('#popup-hide-people')
+const popupPeopleVisible = document.querySelector('#people')
+
+const popupService = document.querySelector('#popup-hide-service')
+const popupServiceVisible = document.querySelector('#service')
+
+//фунция для закрытия развернутого в меню
+function closeAll(esc) {
+  if (esc.classList.contains('popup__icon_reverse')) {
+    esc.classList.remove('popup__icon_reverse')
+  }
+  if (esc.classList.contains('popup__hide_visible')) {
+    esc.classList.remove('popup__hide_visible')
+  }
+}
+
+//функция переворачивания
+function buttonReverse(reverse) {
+  if (reverse.classList.contains('popup__icon_reverse')) {
+  reverse.classList.remove('popup__icon_reverse');
+} else {
+  reverse.classList.add('popup__icon_reverse');
+}
+}
+
+// переворачивание
+popupProjectVisible.addEventListener('click', function () {
+  buttonReverse(popupProjectVisible);
+})
+popupPeopleVisible.addEventListener('click', function () {
+  buttonReverse(popupPeopleVisible);
+})
+popupServiceVisible.addEventListener('click', function () {
+  buttonReverse(popupServiceVisible);
+})
+
+
+//функция разворачивания и сворачивания
+function visibleHide(hide) {
+  if (hide.classList.contains('popup__hide_visible')) {
+  hide.classList.remove('popup__hide_visible');
+} else {
+  hide.classList.add('popup__hide_visible');
+}
+}
+
+// разворачивания и сворачивания при нажатии
+popupProjectVisible.addEventListener('click', function () {
+  visibleHide(popupProject);
+})
+popupPeopleVisible.addEventListener('click', function () {
+  visibleHide(popupPeople);
+})
+popupServiceVisible.addEventListener('click', function () {
+  visibleHide(popupService);
+})
+
+
+
+//функции открытия/закрытия
+function switchPopup(popup) {
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened');
+    closeAll(popupProjectVisible);
+    closeAll(popupPeopleVisible);
+    closeAll(popupServiceVisible);
+    closeAll(popupProject);
+    closeAll(popupPeople);
+    closeAll(popupService);
+    icon.src = '../images/menu-icon.svg'
+  } else {
+  popup.classList.add('popup_opened');
+  icon.src = '../images/Menu-icon-close.jpg';
+  }
+}
+
+// открытие/закрытие попапа на кнопку
+popupMenuSwitch.addEventListener('click', function () {
+  switchPopup(popupMenu);
+})
+
+
 
 function createBox(nameBox, textBox) {
   const newElement = boxTemplate.querySelector('.advantages__box').cloneNode(true);
