5e934dd5465cbdda6e36e2e215c9845817a99698
Denis Ivanov <site.ivanov201@yandex.ru>
2023-01-22 08:40:55 +0300

    21

--- scripts/script.js
+++ scripts/script.js
@@ -14,9 +14,6 @@ const popupPeopleVisible = document.querySelector('#people')
 const popupService = document.querySelector('#popup-hide-service')
 const popupServiceVisible = document.querySelector('#service')
 
-const boxTemplate = document.querySelector('#element-template').content;
-const elementsBox = document.querySelector('.advantages__container');
-
 function createBox(nameBox, textBox) {
   const newElement = boxTemplate.querySelector('.advantages__box').cloneNode(true);
   newElement.querySelector('.advantages__name').textContent = nameBox;
@@ -126,38 +123,3 @@ function switchPopup(popup) {
 popupMenuSwitch.addEventListener('click', function () {
   switchPopup(popupMenu);
 })
-
-
-
-function createBox(nameBox, textBox) {
-  const newElement = boxTemplate.querySelector('.advantages__box').cloneNode(true);
-  newElement.querySelector('.advantages__name').textContent = nameBox;
-  newElement.querySelector('.advantages__text').textContent = textBox;
-  const textContainer = newElement.querySelector('.advantages__text');
-  function openText(textContainer) {
-    let TextBoxOpen = document.querySelector('.advantages__text_active');
-    let buttonBoxActive = document.querySelector('.advantages__button_active');
-    if (document.querySelector('.advantages__text_active')) {
-      TextBoxOpen.classList.remove('advantages__text_active');
-      buttonBoxActive.classList.remove('advantages__button_active');
-    }
-    textContainer.classList.add('advantages__text_active');
-    newElement.querySelector('.advantages__button').classList.add('advantages__button_active');
-  }
-  function closeText(textContainer) {
-    textContainer.classList.remove('advantages__text_active');
-    newElement.querySelector('.advantages__button').classList.remove('advantages__button_active');
-  }
-  newElement.addEventListener('click', function () {
-    if (!newElement.querySelector('.advantages__text_active')) {
-      openText(textContainer);
-    }
-    else {
-      closeText(textContainer);
-    }
-  });
-  return newElement;
-}
-initialBoxes.forEach(function (item) {
-  elementsBox.append(createBox(item.name, item.text));
-});
